54b152f2f48faa50e7d56f7df8eca036af8d0bc4
Muhammad Farooq <farooqayub646@gmail.com>
2024-12-14 14:50:12 +0500

    updated code

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Determine the folder based on the file type
+          if (/\.(css)$/.test(assetInfo.name)) {
+            return 'assets/css/[name].[hash][extname]';
+          }
+          if (/\.(woff|woff2|eot|ttf|otf)$/.test(assetInfo.name)) {
+            return 'assets/fonts/[name].[hash][extname]';
+          }
+          if (/\.(png|jpe?g|gif|svg|webp|avif)$/.test(assetInfo.name)) {
+            return 'assets/images/[name].[hash][extname]';
+          }
+          return '[name].[hash][extname]'; // Default for other assets
+        }
+      }
+    }
+  }
 })
